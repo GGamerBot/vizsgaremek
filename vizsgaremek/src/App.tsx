8a62f83bf46909311c505882289c4614f7673287
A.Bence <144451183+GGamerBot@users.noreply.github.com>
2025-02-18 14:48:02 +0100

    Update App.tsx

--- vizsgaremek/src/App.tsx
+++ vizsgaremek/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import CYOAGame from './components/CYOAGame';
-
-function App() {
-    return (
-        <div className="App">
-            <CYOAGame />
-        </div>
-    );
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ViewScreen from './components/ViewScreen';
+import EditScreen from './components/EditScreen';
+import ConnectionTable from './components/ConnectionTable';
+ 
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/view/:id" element={<ViewScreen />} />
+          <Route path="/edit/:id" element={<EditScreen />} />
+          <Route path="/connections/:gameId" element={<ConnectionTable />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
